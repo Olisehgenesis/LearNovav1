38a1cc011bc38ad5d8a7f118f893f4834cdf5626
Oliseh Genesis <studyug@hotmail.com>
2024-10-09 12:11:58 +0300

    enclosed get account in function to fix : ERROR: Top-level await is not available in the configured target environment

--- src/apps/token/hook/client.ts
+++ src/apps/token/hook/client.ts
@@ -1,16 +1,21 @@
-import { createWalletClient,createPublicClient, custom, http } from 'viem'
+import { createWalletClient, createPublicClient, custom, http } from 'viem'
 import { baseSepolia } from 'viem/chains'
- 
+
 export const publicClient = createPublicClient({
   chain: baseSepolia,
   transport: http()
 })
- 
+
 export const walletClient = createWalletClient({
   chain: baseSepolia,
   transport: custom(window.ethereum)
 })
- 
-// JSON-RPC Account
-export const [account] = await walletClient.getAddresses()
-// Local Account
+
+// Function to get the account address
+export async function getAccount() {
+  const [account] = await walletClient.getAddresses()
+  return account
+}
+
+//export account
+export const account = getAccount
