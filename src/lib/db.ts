952bfcdf789e308ca448597787cdf27c1718b23c
Oliseh Genesis <studyug@hotmail.com>
2024-10-13 08:03:58 +0300

    added base in vite config for fleek to locate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+console.log("superbase key", supabaseKey)
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Helper function to handle Supabase errors
